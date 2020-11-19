294257b433b857eef78e47a728cb12864a7b9046
Jasonlee6789 <jeffersonlee@163.com>
2020-11-19 09:51:29 +0800

    完成了list组件布局用来显示博客列表的基本样式。

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Head from "next/head";
 // import styles from "../styles/Home.module.css";
+import "../styles/pages/index.css";
 import { Row, Col, List, Icon } from "antd";
 import Header from "../components/Header";
 
@@ -46,6 +47,17 @@ const Home = () => {
               renderItem={(item) => (
                 <List.Item>
                   <div className="list-title">{item.title}</div>
+                  <div className="list-icon">
+                    <span>
+                      <Icon type="calender" /> 2020-11-22
+                    </span>
+                    <span>
+                      <Icon type="folder" /> 视频教程
+                    </span>
+                    <span>
+                      <Icon type="calender" /> 1人
+                    </span>
+                  </div>
                   <div className="list-context">{item.context}</div>
                 </List.Item>
               )}
